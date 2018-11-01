6b65214d26132016e2abd90bc3b41aa43d169589
Justin Torrance <justin.torrance91@gmail.com>
2018-11-01 16:49:04 -0600

    Add data set

---
+++ data.js
@@ -0,0 +1,274 @@
+{
+  "questionBank": [
+    {
+      "question": "A program that executes JavaScript code. Most commonly used in web browsers.",
+      "answers": [
+        {"answer": "Interpreter", "isTrue": true},
+        {"answer": "Compiler", "isTrue": false},
+        {"answer": "Operating System", "isTrue": false}
+      ],
+      "image": ""
+    },
+    {
+      "question": "A program that executes JavaScript code. Most commonly used in web browsers.",
+      "answers": [
+        {"answer": "Interpreter", "isTrue": true},
+        {"answer": "Compiler", "isTrue": false},
+        {"answer": "Operating System", "isTrue": false}
+      ],
+      "image": ""
+    },
+    {
+      "question": "A program that executes JavaScript code. Most commonly used in web browsers.",
+      "answers": [
+        {"answer": "Interpreter", "isTrue": true},
+        {"answer": "Compiler", "isTrue": false},
+        {"answer": "Operating System", "isTrue": false}
+      ],
+      "image": ""
+    },
+    {
+      "question": "A program that executes JavaScript code. Most commonly used in web browsers.",
+      "answers": [
+        {"answer": "Interpreter", "isTrue": true},
+        {"answer": "Compiler", "isTrue": false},
+        {"answer": "Operating System", "isTrue": false}
+      ],
+      "image": ""
+    },
+    {
+      "question": "A program that executes JavaScript code. Most commonly used in web browsers.",
+      "answers": [
+        {"answer": "Interpreter", "isTrue": true},
+        {"answer": "Compiler", "isTrue": false},
+        {"answer": "Operating System", "isTrue": false}
+      ],
+      "image": ""
+    },
+    {
+      "question": "A program that executes JavaScript code. Most commonly used in web browsers.",
+      "answers": [
+        {"answer": "Interpreter", "isTrue": true},
+        {"answer": "Compiler", "isTrue": false},
+        {"answer": "Operating System", "isTrue": false}
+      ],
+      "image": ""
+    },
+    {
+      "question": "A program that executes JavaScript code. Most commonly used in web browsers.",
+      "answers": [
+        {"answer": "Interpreter", "isTrue": true},
+        {"answer": "Compiler", "isTrue": false},
+        {"answer": "Operating System", "isTrue": false}
+      ],
+      "image": ""
+    },
+    {
+      "question": "A program that executes JavaScript code. Most commonly used in web browsers.",
+      "answers": [
+        {"answer": "Interpreter", "isTrue": true},
+        {"answer": "Compiler", "isTrue": false},
+        {"answer": "Operating System", "isTrue": false}
+      ],
+      "image": ""
+    },
+    {
+      "question": "A program that executes JavaScript code. Most commonly used in web browsers.",
+      "answers": [
+        {"answer": "Interpreter", "isTrue": true},
+        {"answer": "Compiler", "isTrue": false},
+        {"answer": "Operating System", "isTrue": false}
+      ],
+      "image": ""
+    },
+    {
+      "question": "A program that executes JavaScript code. Most commonly used in web browsers.",
+      "answers": [
+        {"answer": "Interpreter", "isTrue": true},
+        {"answer": "Compiler", "isTrue": false},
+        {"answer": "Operating System", "isTrue": false}
+      ],
+      "image": ""
+    },
+    {
+      "question": "A program that executes JavaScript code. Most commonly used in web browsers.",
+      "answers": [
+        {"answer": "Interpreter", "isTrue": true},
+        {"answer": "Compiler", "isTrue": false},
+        {"answer": "Operating System", "isTrue": false}
+      ],
+      "image": ""
+    },
+    {
+      "question": "A program that executes JavaScript code. Most commonly used in web browsers.",
+      "answers": [
+        {"answer": "Interpreter", "isTrue": true},
+        {"answer": "Compiler", "isTrue": false},
+        {"answer": "Operating System", "isTrue": false}
+      ],
+      "image": ""
+    },
+    {
+      "question": "A program that executes JavaScript code. Most commonly used in web browsers.",
+      "answers": [
+        {"answer": "Interpreter", "isTrue": true},
+        {"answer": "Compiler", "isTrue": false},
+        {"answer": "Operating System", "isTrue": false}
+      ],
+      "image": ""
+    },
+    {
+      "question": "A program that executes JavaScript code. Most commonly used in web browsers.",
+      "answers": [
+        {"answer": "Interpreter", "isTrue": true},
+        {"answer": "Compiler", "isTrue": false},
+        {"answer": "Operating System", "isTrue": false}
+      ],
+      "image": ""
+    },
+    {
+      "question": "A program that executes JavaScript code. Most commonly used in web browsers.",
+      "answers": [
+        {"answer": "Interpreter", "isTrue": true},
+        {"answer": "Compiler", "isTrue": false},
+        {"answer": "Operating System", "isTrue": false}
+      ],
+      "image": ""
+    },
+    {
+      "question": "A program that executes JavaScript code. Most commonly used in web browsers.",
+      "answers": [
+        {"answer": "Interpreter", "isTrue": true},
+        {"answer": "Compiler", "isTrue": false},
+        {"answer": "Operating System", "isTrue": false}
+      ],
+      "image": ""
+    },
+    {
+      "question": "A program that executes JavaScript code. Most commonly used in web browsers.",
+      "answers": [
+        {"answer": "Interpreter", "isTrue": true},
+        {"answer": "Compiler", "isTrue": false},
+        {"answer": "Operating System", "isTrue": false}
+      ],
+      "image": ""
+    },
+    {
+      "question": "A program that executes JavaScript code. Most commonly used in web browsers.",
+      "answers": [
+        {"answer": "Interpreter", "isTrue": true},
+        {"answer": "Compiler", "isTrue": false},
+        {"answer": "Operating System", "isTrue": false}
+      ],
+      "image": ""
+    },
+    {
+      "question": "A program that executes JavaScript code. Most commonly used in web browsers.",
+      "answers": [
+        {"answer": "Interpreter", "isTrue": true},
+        {"answer": "Compiler", "isTrue": false},
+        {"answer": "Operating System", "isTrue": false}
+      ],
+      "image": ""
+    },
+    {
+      "question": "A program that executes JavaScript code. Most commonly used in web browsers.",
+      "answers": [
+        {"answer": "Interpreter", "isTrue": true},
+        {"answer": "Compiler", "isTrue": false},
+        {"answer": "Operating System", "isTrue": false}
+      ],
+      "image": ""
+    },
+    {
+      "question": "A program that executes JavaScript code. Most commonly used in web browsers.",
+      "answers": [
+        {"answer": "Interpreter", "isTrue": true},
+        {"answer": "Compiler", "isTrue": false},
+        {"answer": "Operating System", "isTrue": false}
+      ],
+      "image": ""
+    },
+    {
+      "question": "A program that executes JavaScript code. Most commonly used in web browsers.",
+      "answers": [
+        {"answer": "Interpreter", "isTrue": true},
+        {"answer": "Compiler", "isTrue": false},
+        {"answer": "Operating System", "isTrue": false}
+      ],
+      "image": ""
+    },
+    {
+      "question": "A program that executes JavaScript code. Most commonly used in web browsers.",
+      "answers": [
+        {"answer": "Interpreter", "isTrue": true},
+        {"answer": "Compiler", "isTrue": false},
+        {"answer": "Operating System", "isTrue": false}
+      ],
+      "image": ""
+    },
+    {
+      "question": "A program that executes JavaScript code. Most commonly used in web browsers.",
+      "answers": [
+        {"answer": "Interpreter", "isTrue": true},
+        {"answer": "Compiler", "isTrue": false},
+        {"answer": "Operating System", "isTrue": false}
+      ],
+      "image": ""
+    },
+    {
+      "question": "A program that executes JavaScript code. Most commonly used in web browsers.",
+      "answers": [
+        {"answer": "Interpreter", "isTrue": true},
+        {"answer": "Compiler", "isTrue": false},
+        {"answer": "Operating System", "isTrue": false}
+      ],
+      "image": ""
+    },
+    {
+      "question": "A program that executes JavaScript code. Most commonly used in web browsers.",
+      "answers": [
+        {"answer": "Interpreter", "isTrue": true},
+        {"answer": "Compiler", "isTrue": false},
+        {"answer": "Operating System", "isTrue": false}
+      ],
+      "image": ""
+    },
+    {
+      "question": "A program that executes JavaScript code. Most commonly used in web browsers.",
+      "answers": [
+        {"answer": "Interpreter", "isTrue": true},
+        {"answer": "Compiler", "isTrue": false},
+        {"answer": "Operating System", "isTrue": false}
+      ],
+      "image": ""
+    },
+    {
+      "question": "A program that executes JavaScript code. Most commonly used in web browsers.",
+      "answers": [
+        {"answer": "Interpreter", "isTrue": true},
+        {"answer": "Compiler", "isTrue": false},
+        {"answer": "Operating System", "isTrue": false}
+      ],
+      "image": ""
+    },
+    {
+      "question": "A program that executes JavaScript code. Most commonly used in web browsers.",
+      "answers": [
+        {"answer": "Interpreter", "isTrue": true},
+        {"answer": "Compiler", "isTrue": false},
+        {"answer": "Operating System", "isTrue": false}
+      ],
+      "image": ""
+    },
+    {
+      "question": "A program that executes JavaScript code. Most commonly used in web browsers.",
+      "answers": [
+        {"answer": "Interpreter", "isTrue": true},
+        {"answer": "Compiler", "isTrue": false},
+        {"answer": "Operating System", "isTrue": false}
+      ],
+      "image": ""
+    }
+  ]
+}
